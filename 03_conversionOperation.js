2d01ce5d24902ba21a1bbf8f88b987c99c18902c
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-15 01:08:59 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueInNumber =Number(score)
+console.log( valueInNumber);
+
+
+//"33=> 33"
+//"33abc"=>NaN
+//true=>1; false =>0
+
+
+letisLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1=> true; 0=> false
+//"" =>false
+//"anurag"=> ture
+
+let someNumber = 33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
