2cb3c913e33773118cea7d86de27b90ecba719e5
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-15 00:38:03 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "nikhilpandey1510@gmail.com"
+var accountPassword = "748805"
+accountCity = "mumbai"
+
+// accountId =2   not allowed
+
+console.log(accountId);
+
+let
+
+
+
+    accountEmail = "hd@gmail.com"
+accountPassword = "21211"
+accountCity = "banaras"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+//prefer not to use var 
+/*
+bcoz of the issue in block scope and functional scope*/
+
