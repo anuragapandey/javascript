7f981d7e1786b1767a113900c75ed8abe723f9f5
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-15 00:38:32 +0530

    Add files via upload

---
+++ 02_dataTyes.js
@@ -0,0 +1,40 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using node.js not browser
+
+console.log(3+3);console.log("aman")
+
+// semicolon use for do not using the next line otherwise we dont need of this 
+
+console.log(3
+    +
+    3)   // this code will run but this code readability should high   
+
+console.log("anurag")
+
+//  about the data types 
+
+let name = anurag // string 
+let age =  18 // numbers 
+let isLoggedIn = false // boolean 
+  
+
+
+
+// number range => 2 to power 53
+// bigint 
+//string=> " "
+// boolean => true/ false 
+// null => stand alone value (empty) , null is object 
+// undefined => 
+// symbol =>unique 
+
+
+// object
+
+console.log(typeof "anu")
+console.log(typeof "null") // objct
+console.log(typeof "undefined") // undefined
+
+
+
