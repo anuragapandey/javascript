9be1223f268c1dcbca178fca47aaccaf3a602e0a
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-15 12:35:05 +0530

    Add files via upload

---
+++ 05_comparison.js
@@ -0,0 +1,24 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2 != 1)
+
+
+console.log("2">1);
+console.log("02">1)*/
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0)
+
+console.log(undefined==0);
+console.log(undefined>0)
+
+// always avoit this type of code
+
+
+// === strik check
+
+
+
