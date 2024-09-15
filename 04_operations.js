53f2edaa0ba3dfe3f7db5f4b270d8eb01b1c773e
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-15 11:58:35 +0530

    Add files via upload

---
+++ 04_operations.js
@@ -0,0 +1,35 @@
+/*"
+let str2 = "anurag"
+
+let str3 = str1 +str2 
+console.log(str3);
+
+console.log ("1" + 2)
+console.log (1+"2")
+console.log("1"+"2"+3)
+
+console.log("1"+2+2)
+console.log(3+5*5%3)
+
+
+console.log(true)
+console(+"")
+console(+true)
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2*/
+
+let gameCounter= 100
+++gameCounter;
+console.log(gameCounter);
+
+//
+
+
+
+
+
+
+
