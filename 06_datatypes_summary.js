26f410710008b9187467fb127f7fb7349b4a5c1f
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-15 15:17:30 +0530

    Add files via upload

---
+++ 06_datatypes_summary.js
@@ -0,0 +1,26 @@
+// primitive
+// 7 types: string, number, boolean , null, undefined, symbol,bigint , 
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+const id = Symbol('123')
+const anotherId =symbol('123')
+const bigNumber =34465242n
+
+// non- primitive (reference)
+
+// array, objects, functions 
+
+/*const heros = ["ashishpandey", "aman","a2"]
+
+let myObj {
+    name:"anurag"
+    age:22 ,
+}
+const myFunction = function(){
+console.log("hello");
+}
+console.log(typeof bigNumber)
+
