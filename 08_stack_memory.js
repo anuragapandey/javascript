2d10f7f3b3d8dd512f8cf2c5d6cf471ce0f74c08
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-16 00:04:03 +0530

    Add files via upload

---
+++ 08_stack_memory.js
@@ -0,0 +1,18 @@
+// stack(primitive), heap(non-primitive)
+
+let myYoutubename ="a2_pandey"
+let anothername= myYoutubename
+anothername="coffee with aman"
+
+console.log(anothername)
+console.log(anothername)
+
+let userOne ={
+    email:"user@google.com",
+    upi:"user@vbl"
+}
+
+let userTwo =userOne
+userTwo.email="hitesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
